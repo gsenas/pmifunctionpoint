42fef4e2629f9a0a30a273c7652532c175a4287c
Guillermo Señas <gsenas@gmail.com>
2020-02-07 00:09:28 +0100

    Se pueden crear Departamentos
    Took 1 hour 16 minutes

--- lib/tabs/departamentos.dart
+++ lib/tabs/departamentos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import '../globals.dart';
 
 class DepartamentosTab extends StatefulWidget {
@@ -11,13 +12,53 @@ class DepartamentosTab extends StatefulWidget {
 }
 
 class _DepartamentosTabState extends State<DepartamentosTab> {
+  TextEditingController _textFieldController = TextEditingController();
+
+  Future<void> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('Crear nuevo'),
+            content: CupertinoTextField(
+              autofocus: true,
+              controller: _textFieldController,
+              clearButtonMode: OverlayVisibilityMode.editing,
+              placeholder: 'Nombre del departamento',
+              padding: EdgeInsets.all(10),
+            ),
+            actions: <Widget>[
+              new CupertinoButton(
+                child: new Text('Cancelar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              new CupertinoButton(
+                child: new Text('Aceptar'),
+                onPressed: () {
+                  if (_textFieldController.text.toString() != '') {
+                    setState(() {
+                      departamentos
+                          .add(Departamento(_textFieldController.text, 0));
+                      _textFieldController.clear();
+                      Navigator.of(context).pop();
+                    });
+                  }
+                },
+              )
+            ],
+          );
+        });
+  }
+
   Widget build(BuildContext context) {
     return new Scaffold(
         body: _buildListaDptos(),
         floatingActionButton: FloatingActionButton(
             backgroundColor: Colors.deepOrange,
-            //onPressed: _incrementCounter,
-            tooltip: 'Increment',
+            onPressed: () => _displayDialog(context),
+            tooltip: 'Añadir un nuevo departamento',
             child: Icon(Icons.add)));
   }
 
