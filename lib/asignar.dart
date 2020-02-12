aac6789e364fda725c21f80fa117aea5f064fed0
Guillermo Señas <gsenas@gmail.com>
2020-02-12 15:25:51 +0100

    Tercer borrador de asignación de departamento a empleados
    Took 29 minutes

--- lib/asignar.dart
+++ lib/asignar.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'globals.dart';
+
+String _asignarE() {
+  return "Hola";
+}
 
 void asignarEmpleadoDpto(idEmpleado, idDpto, context) {
   Navigator.of(context).push(
@@ -6,65 +11,97 @@ void asignarEmpleadoDpto(idEmpleado, idDpto, context) {
       builder: (BuildContext context) {
         return new Scaffold(
           appBar: AppBar(
-              backgroundColor: Colors.deepPurple,
+            backgroundColor: Colors.deepPurple,
             title: Text('Asignar empleado a departamento'),
             actions: <Widget>[
               FlatButton(
-                child: Text('GUARDAR', style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                )),
+                child: Text('GUARDAR',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    )),
                 onPressed: () {
                   //Navigator.pop(context, DismissDialogAction.save);
                   Navigator.pop(context, null);
                 },
               ),
             ],
-            ),
+          ),
           body: Form(
             onWillPop: null,
             child: Scrollbar(
               child: ListView(
-                padding: const EdgeInsets.all(16.0),
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    alignment: Alignment.bottomLeft,
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Event name',
-                        filled: true,
+                  padding: const EdgeInsets.all(16.0),
+                  children: <Widget>[
+                    ListTile(
+                      title: const Text('Empleado:'),
+                      trailing: DropdownButton<String>(
+                        value: 'One',
+                        onChanged: (String e) {
+                          _asignarE();
+                        },
+                        items: <String>[
+                          'One',
+                          'Two',
+                          'Free',
+                          'Four',
+                          'Can',
+                          'I',
+                          'Have',
+                          'A',
+                          'Little',
+                          'Bit',
+                          'More',
+                          'Five',
+                          'Six',
+                          'Seven',
+                          'Eight',
+                          'Nine',
+                          'Ten',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
                       ),
-
-                      onChanged: null,
                     ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    alignment: Alignment.bottomLeft,
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Location',
-                        hintText: 'Where is the event?',
-                        filled: true,
+                    ListTile(
+                      title: const Text('Departamento:'),
+                      trailing: DropdownButton<String>(
+                        value: 'One',
+                        onChanged: (String e) {
+                          _asignarE();
+                        },
+                        items: <String>[
+                          'One',
+                          'Two',
+                          'Free',
+                          'Four',
+                          'Can',
+                          'I',
+                          'Have',
+                          'A',
+                          'Little',
+                          'Bit',
+                          'More',
+                          'Five',
+                          'Six',
+                          'Seven',
+                          'Eight',
+                          'Nine',
+                          'Ten',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
                       ),
-                      onChanged: null,
-                    ),
-                  ),
-                ]
-                    .map<Widget>((Widget child) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    height: 96.0,
-                    child: child,
-                  );
-                })
-                    .toList(),
-                ),
+                    )
+                  ]),
             ),
           ),
-
-
         );
       },
     ),
